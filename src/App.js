ffb25714ea735ebdb2e9631aa1676128216a3704
smarttin <smarttin77@gmail.com>
2022-01-19 00:41:30 +0100

    Route tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import UserPage from './pages/UserPage';
 import LanguageSelector from './components/LanguageSelector';
+import AccountActivationPage from './pages/AccountActivationPage';
+import { useTranslation } from 'react-i18next';
+import logo from './assets/hoaxify.png';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
-    <>
-      <div className="container pt-3">
-        <SignUpPage />
+    <Router>
+      <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
+        <div className="container">
+          <Link className="navbar-brand" to="/" title="Home">
+            <img src={logo} alt="Hoaxify" width="60" />
+            Hoaxify
+          </Link>
+          <ul className="navbar-nav">
+            <Link className="nav-link" to="/signup">
+              {t('signUp')}
+            </Link>
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </ul>
+        </div>
+      </nav>
+      <div className="container">
+        <Route exact path="/" component={HomePage} />
+        <Route path="/signup" component={SignUpPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/user/:id" component={UserPage} />
+        <Route path="/activate/:token" component={AccountActivationPage} />
         <LanguageSelector />
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/App.spec.js
@@ -0,0 +1,87 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+describe('Routing', () => {
+  const setup = (path) => {
+    window.history.pushState({}, '', path);
+    render(<App />);
+  };
+  it.each`
+    path               | pageTestId
+    ${'/'}             | ${'home-page'}
+    ${'/signup'}       | ${'signup-page'}
+    ${'/login'}        | ${'login-page'}
+    ${'/user/1'}       | ${'user-page'}
+    ${'/user/2'}       | ${'user-page'}
+    ${'/activate/123'} | ${'activation-page'}
+    ${'/activate/456'} | ${'activation-page'}
+  `('displays $pageTestId when path is $path', ({ path, pageTestId }) => {
+    setup(path);
+    const page = screen.queryByTestId(pageTestId);
+    expect(page).toBeInTheDocument();
+  });
+
+  it.each`
+    path               | pageTestId
+    ${'/'}             | ${'signup-page'}
+    ${'/'}             | ${'login-page'}
+    ${'/'}             | ${'user-page'}
+    ${'/'}             | ${'activation-page'}
+    ${'/signup'}       | ${'home-page'}
+    ${'/signup'}       | ${'login-page'}
+    ${'/signup'}       | ${'user-page'}
+    ${'/signup'}       | ${'activation-page'}
+    ${'/login'}        | ${'home-page'}
+    ${'/login'}        | ${'signup-page'}
+    ${'/login'}        | ${'user-page'}
+    ${'/login'}        | ${'activation-page'}
+    ${'/user/1'}       | ${'home-page'}
+    ${'/user/1'}       | ${'signup-page'}
+    ${'/user/1'}       | ${'login-page'}
+    ${'/user/1'}       | ${'activation-page'}
+    ${'/activate/123'} | ${'home-page'}
+    ${'/activate/123'} | ${'signup-page'}
+    ${'/activate/123'} | ${'login-page'}
+    ${'/activate/123'} | ${'user-page'}
+  `('does not display $pageTestId when path is $path', ({ path, pageTestId }) => {
+    setup(path);
+    const page = screen.queryByTestId(pageTestId);
+    expect(page).not.toBeInTheDocument();
+  });
+
+  it.each`
+    targetPage
+    ${'Home'}
+    ${'Sign Up'}
+    ${'Login'}
+  `('has link to $targetPage on NavBar', ({ targetPage }) => {
+    setup('/');
+    const link = screen.getByRole('link', { name: targetPage });
+    expect(link).toBeInTheDocument();
+  });
+
+  it.each`
+    initialPath  | clickingTo   | visiblePage
+    ${'/'}       | ${'Sign Up'} | ${'signup-page'}
+    ${'/signup'} | ${'Home'}    | ${'home-page'}
+    ${'/signup'} | ${'Login'}   | ${'login-page'}
+  `(
+    'displays $visiblePage after clicking $clickingTo',
+    ({ initialPath, clickingTo, visiblePage }) => {
+      setup(initialPath);
+      const link = screen.getByRole('link', { name: clickingTo });
+      userEvent.click(link);
+      expect(screen.getByTestId(visiblePage)).toBeInTheDocument();
+    },
+  );
+
+  it('displays home page when clicking brand logo', () => {
+    setup('/login');
+    const logo = screen.queryByAltText('Hoaxify');
+    userEvent.click(logo);
+    expect(screen.getByTestId('home-page')).toBeInTheDocument();
+  });
+});
