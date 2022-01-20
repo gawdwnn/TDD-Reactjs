ad939052ddc27d42b264e87074606f7f149d5aec
smarttin <smarttin77@gmail.com>
2022-01-20 15:28:08 +0100

    user layout tests

--- src/App.spec.js
+++ src/App.spec.js
@@ -26,6 +26,17 @@ const server = setupServer(
       }),
     );
   }),
+  rest.get('/api/1.0/users/:id', (req, res, ctx) => {
+    const id = Number.parseInt(req.params.id);
+    return res(
+      ctx.json({
+        id: id,
+        username: 'user' + id,
+        email: 'user' + id + '@mail.com',
+        image: null,
+      }),
+    );
+  }),
 );
 
 beforeEach(() => {
